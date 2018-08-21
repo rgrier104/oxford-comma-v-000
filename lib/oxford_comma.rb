149497b0a8c7b7f29aec1868dcdebda140920088
Rachel Grier <rgrier104@gmail.com>
2018-08-21 23:30:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    short_array = array.pop
+    short_string = array.join(", ")
+    short_string << ", and #{short_array}"
+    return short_string
+  end
 end
