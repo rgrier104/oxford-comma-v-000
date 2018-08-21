30646e1d8d860c02f3b08fb5855d167ef52ce187
Rachel Grier <rgrier104@gmail.com>
2018-08-21 23:25:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
-  last_index = array.length-1
+  if array.length = 1
+    return array[0]
+  elsif array.length = 2
+    return array.join(" and ")
   short_array = array.pop
   short_string = array.join(", ")
   short_string << "and #{short_array}"
 end
+
+if length = 1, string = array
