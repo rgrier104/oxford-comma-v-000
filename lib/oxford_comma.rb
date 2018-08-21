1790da21e8231c423adfdd96a1c4f4eb2c45c978
Rachel Grier <rgrier104@gmail.com>
2018-08-21 23:23:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,5 @@ def oxford_comma(array)
   last_index = array.length-1
   short_array = array.pop
   short_string = array.join(", ")
-  short_string << "and short_array"
+  short_string << "and #{short_array}"
 end
